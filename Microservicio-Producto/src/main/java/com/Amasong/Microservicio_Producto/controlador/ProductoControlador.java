34a9a565bb7726896d9b2be46c988a4de5ab94ed
Benjamin David Espinoza Rojas <benjamindavid@MacBook-Pro-de-Benjamin.local>
2025-05-31 01:02:31 -0400

    mini cambio

--- Microservicio-Producto/src/main/java/com/Amasong/Microservicio_Producto/controlador/ProductoControlador.java
+++ Microservicio-Producto/src/main/java/com/Amasong/Microservicio_Producto/controlador/ProductoControlador.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/productos")
+@RequestMapping("/api/Producto")
 public class ProductoControlador {
 
     @Autowired
