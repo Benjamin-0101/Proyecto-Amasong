00bbf5756572bb65738bd54dcfb1eb82264085f1
jxzeayz <arma.calderon@duocuc.cl>
2025-06-13 20:43:43 -0400

    Update MicroservicioSesionApplicationTests.java
    cambio en MicroservicioReporteApplicationTests

--- Microservicio-Sesion/src/test/java/com/Amasong/Microservicio_Sesion/MicroservicioSesionApplicationTests.java
+++ Microservicio-Sesion/src/test/java/com/Amasong/Microservicio_Sesion/MicroservicioSesionApplicationTests.java
@@ -1,13 +1,15 @@
-package com.Amasong.Microservicio_Sesion;
+package com.Amasong.Microservicio_Reporte;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootTest
-class MicroservicioSesionApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootApplication
+public class MicroservicioReporteApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioReporteApplicationTests.class, args);
+    }
 }
